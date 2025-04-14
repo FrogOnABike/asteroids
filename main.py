8e4536ff1147fbca45b807cd8d3ca9b713d5f545
FrogOnABike <mark.skrzypczyk.com>
2025-04-14 11:20:40 +0100

    Craft added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +11,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen_timer = pygame.time.Clock()
+    dt = 0
+
+    craft = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill("black")
+        craft.draw(screen)
         pygame.display.flip()
-
+        screen_timer.tick(60)
+        dt = screen_timer.tick()/1000
 
 
 if __name__ == "__main__":
